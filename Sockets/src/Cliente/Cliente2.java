0d355d0a1b1cec0753308e0ce03d2a9f301df940
Luan H Alves <luan.henrique20@gmail.com>
2019-06-08 19:11:16 -0300

    Modificações no servidor socket

--- Sockets/src/Cliente/Cliente2.java
+++ Sockets/src/Cliente/Cliente2.java
@@ -1,36 +1,33 @@
 package Cliente;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
 import java.util.Scanner;
 
-public class Cliente {
+public class Cliente2 {
 
-//    private DataInputStream in;
-//    private DataOutputStream out;
+    private DataInputStream in;
+    private DataOutputStream out;
 
     public static void main(String[] args) {
 
         String host = "127.0.0.1";
-        int portaServidor = 7896;
+        int portaServidor = 666;
         try (Socket socket = new Socket (host, portaServidor)) {
             PrintWriter output = new PrintWriter (socket.getOutputStream ( ), true);
             BufferedReader input = new BufferedReader (new InputStreamReader (socket.getInputStream ( )));
             Scanner scanner = new Scanner (System.in);
 
-//            DataInputStream in= new DataInputStream(socket.getInputStream());
-//            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in= new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
             String mensagem = null;
             while (!"exit".equalsIgnoreCase (mensagem)) {
                 mensagem = scanner.nextLine ( );
-                output.println (mensagem);
-                output.flush ( );
-//                out.writeUTF(mensagem);
-                System.out.println (input.readLine ());
+//                output.println (mensasgem);
+//                output.flush ( );
+                out.writeUTF(mensagem);
+                System.out.println (in.readUTF ( ));
             }
             scanner.close ( );
         } catch (IOException e) {
--- Sockets/src/Servidor/Servidor.java
+++ Sockets/src/Servidor/Servidor.java
@@ -5,18 +5,82 @@ import FigureaGeometrica.FiguraGeometricaImpl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
+
+
+    private static class ClienteConHandler implements Runnable {
+
+        private final Socket clienteSocket;
+
+        ClienteConHandler(Socket socket) {
+            this.clienteSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            PrintWriter output = null;
+            BufferedReader input = null;
+            FiguraGeometricaImpl figura = new FiguraGeometricaImpl ( );
+
+            try {
+
+                output = new PrintWriter (clienteSocket.getOutputStream ( ), true);
+                input = new BufferedReader (new InputStreamReader (clienteSocket.getInputStream ( )));
+
+//                DataInputStream in = new DataInputStream (clienteSocket.getInputStream ( ));
+//                DataOutputStream out = new DataOutputStream (clienteSocket.getOutputStream ( ));
+
+                String msgCliente;
+                while ((msgCliente = input.readLine ( )) != null) {
+//                while ((msgCliente = in.readUTF ( )) != null) {
+
+//                    msgCliente = in.readUTF ();
+                    if (msgCliente.equals ("x")) {
+                        msgCliente = figura.figuraAleatoria ( );
+                        System.out.printf ("Msg Enviado pelo Servidor: %s\n", msgCliente);
+                        output.println (msgCliente);
+//                        out.writeUTF (msgCliente);
+                    } else {
+                        msgCliente = "Requisicao invalida!!!";
+                        System.err.printf ("Msg Enviado pelo Servidor: %s\n", msgCliente);
+                        output.println (msgCliente);
+//                        out.writeUTF (msgCliente);
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace ( );
+            } finally {
+
+                try {
+
+                    if (output != null) {
+                        output.close ( );
+                    }
+                    if (input != null)
+                        input.close ( );
+                    clienteSocket.close ( );
+
+                } catch (IOException e) {
+                    e.printStackTrace ( );
+                }
+
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
 
         ServerSocket servidor = null;
         try {
 
-            servidor = new ServerSocket (666);
+            servidor = new ServerSocket (7896);
             servidor.setReuseAddress (true);
 
             /*
@@ -25,8 +89,11 @@ public class Servidor {
             while (true) {
                 Socket cliente = servidor.accept ( );
                 System.out.println ("Novo cliente conectado " + cliente.getInetAddress ( ).getHostAddress ( ));
-                ClientHandler clienteSock = new ClientHandler (cliente);
+                ClienteConHandler clienteSock = new ClienteConHandler (cliente);
 
+                /*
+                 *  Thread para cada cliente
+                 **/
                 new Thread (clienteSock).start ( );
             }
 
@@ -45,54 +112,4 @@ public class Servidor {
         }
     }
 
-
-    private static class ClientHandler implements Runnable {
-
-        private final Socket clienteSocket;
-
-        private FiguraGeometricaImpl figura = new FiguraGeometricaImpl ( );
-
-        ClientHandler(Socket socket) {
-            this.clienteSocket = socket;
-        }
-
-        @Override
-        public void run() {
-            PrintWriter output = null;
-            BufferedReader input = null;
-            try {
-
-                output = new PrintWriter (clienteSocket.getOutputStream ( ), true);
-                input = new BufferedReader (new InputStreamReader (clienteSocket.getInputStream ( )));
-                String msgCliente;
-                while ((msgCliente = input.readLine ( )) != null) {
-
-                    if (msgCliente.equals ("x")) {
-                        msgCliente = figura.figuraAleatoria ( );
-                        System.out.printf ("Msg Enviado pelo Servidor: %s\n", msgCliente);
-                        output.println (msgCliente);
-                    } else {
-                        msgCliente = "Requisicao invalida!!!";
-                        System.err.printf ("Msg Enviado pelo Servidor: %s\n", msgCliente);
-                        output.println (msgCliente);
-                    }
-
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace ( );
-            } finally {
-                try {
-                    if (output != null) {
-                        output.close ( );
-                    }
-                    if (input != null)
-                        input.close ( );
-                    clienteSocket.close ( );
-                } catch (IOException e) {
-                    e.printStackTrace ( );
-                }
-            }
-        }
-    }
 }
